5bf04edb7bdaec3cab09f3fc93d930260883ba57
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2023-11-19 08:46:33 +0800

    Adding condition
    I add a condition using JS so that when it met a certain `px` (in this code, 800px) it will change the image from "image-product-mobile" to "image-product-desktop"

---
+++ src/script.js
@@ -0,0 +1,11 @@
+const image = document.getElementById("responsiveImage")
+
+function changeImage() {
+  if (window.innerWidth <= 800) {
+    image.src = "./images/image-product-mobile.jpg"
+  } else {
+    image.src = "./images/image-product-desktop.jpg"
+  }
+}
+
+window.addEventListener("resize", changeImage)
