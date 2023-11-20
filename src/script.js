4bf96193e21e4793f2eaefee5d3e37b253c5c320
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2023-11-20 21:23:19 +0800

    Fixing some bug!🛠️
    So I spent my day finding the bug on why for some reason the @media queries doesn't work and also my JS giving an error that said "Image is null"
    
    Upon searching I finally find what is the problem. I didn't put:
    
      document.addEventListener("DOMContentLoaded", function ()
    
    ...to my JS.
    
    with this piece of code, finally my @media-queries are working.
    
    Those background-color that;s in the CSS are only for testing purposes and it will be remove once I've manage to style some more using @media queries

--- src/script.js
+++ src/script.js
@@ -1,11 +1,24 @@
-const image = document.getElementById("responsiveImage")
+document.addEventListener("DOMContentLoaded", function () {
+  const image = document.getElementById("responsiveImage")
 
-function changeImage() {
-  if (window.innerWidth <= 800) {
-    image.src = "./images/image-product-mobile.jpg"
-  } else {
-    image.src = "./images/image-product-desktop.jpg"
+  function changeImage() {
+    if (window.innerWidth <= 800) {
+      image.src = "./images/image-product-mobile.jpg"
+    } else {
+      image.src = "./images/image-product-desktop.jpg"
+    }
   }
-}
 
-window.addEventListener("resize", changeImage)
+  function changeBackgroundColor() {
+    const bgColor = document.querySelector("body")
+
+    if (window.innerWidth <= 800) {
+      bgColor.style.backgroundColor = "white"
+    } else {
+      bgColor.style.backgroundColor = "red"
+    }
+  }
+
+  window.addEventListener("resize", changeImage)
+  window.addEventListener("resize", changeBackgroundColor)
+})
