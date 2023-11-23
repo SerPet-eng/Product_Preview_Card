16ca983d07fe7f850651474098880d44a09273c7
Christian De Guzman <62647434+SerPet-eng@users.noreply.github.com>
2023-11-23 20:43:09 +0800

    Deleting Unnecessary Codes
    So I remove some code to my `script.js` and the reason is that, "It's for sane test only". Also in my `styles.css` where in the @media queries when it met a certain width, the `body` element will change into a `black` color

--- src/script.js
+++ src/script.js
@@ -1,24 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const image = document.getElementById("responsiveImage")
+document.addEventListener('DOMContentLoaded', function () {
+  const image = document.getElementById('responsiveImage');
 
   function changeImage() {
     if (window.innerWidth <= 800) {
-      image.src = "./images/image-product-mobile.jpg"
+      image.src = './images/image-product-mobile.jpg';
     } else {
-      image.src = "./images/image-product-desktop.jpg"
+      image.src = './images/image-product-desktop.jpg';
     }
   }
 
-  function changeBackgroundColor() {
-    const bgColor = document.querySelector("body")
-
-    if (window.innerWidth <= 800) {
-      bgColor.style.backgroundColor = "white"
-    } else {
-      bgColor.style.backgroundColor = "red"
-    }
-  }
-
-  window.addEventListener("resize", changeImage)
-  window.addEventListener("resize", changeBackgroundColor)
-})
+  window.addEventListener('resize', changeImage);
+});
